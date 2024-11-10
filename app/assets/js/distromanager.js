c1cd790615e48c9ed6503ada7520eb9156e6d85c
javierlm032 <javierlm03@gmail.com>
2024-11-10 06:43:04 -0500

    update

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://github.com/javierlm032/nebula/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://www.klar-com.info/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
